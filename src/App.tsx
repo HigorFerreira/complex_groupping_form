cc6ecf21dac3f17a4d4ad8b6fbff972816c701b1
Higor <hfashigor@gmail.com>
2026-04-24 11:04:21 -0300

    Several adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { createForm } from '@/lib/complex_form'
 import './App.css'
+import { useEffect, useRef } from 'react'
+
+const { Provider, useData, useDataOperations } = createForm<'list'>()
 
 function App() {
-	const [count, setCount] = useState(0)
+	const callOnce = useRef(false)
+	const data = useData()
+	const dop = useDataOperations()
 
-	return (
-		<>
-			<section id="center">
-				<div className="hero">
-					<img src={heroImg} className="base" width="170" height="179" alt="" />
-					<img src={reactLogo} className="framework" alt="React logo" />
-					<img src={viteLogo} className="vite" alt="Vite logo" />
-				</div>
-				<div>
-					<h1>Get started</h1>
-					<p>
-						Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-					</p>
-				</div>
-				<button
-					type="button"
-					className="counter"
-					onClick={() => setCount((count) => count + 1)}
-				>
-					Count is {count}
-				</button>
-			</section>
+	useEffect(() => {
+		if(!callOnce.current){
+			dop.append('list', { label: 'Label', value: '001' })
+				.append('list', { label: 'Telefone', value: '002' })
+			callOnce.current = true
+		}
+	}, [ callOnce, dop ])
 
-			<div className="ticks"></div>
+	useEffect(() => console.log({ data }), [ data ])
 
-			<section id="next-steps">
-				<div id="docs">
-					<svg className="icon" role="presentation" aria-hidden="true">
-						<use href="/icons.svg#documentation-icon"></use>
-					</svg>
-					<h2>Documentation</h2>
-					<p>Your questions, answered</p>
-					<ul>
-						<li>
-							<a href="https://vite.dev/" target="_blank">
-								<img className="logo" src={viteLogo} alt="" />
-								Explore Vite
-							</a>
-						</li>
-						<li>
-							<a href="https://react.dev/" target="_blank">
-								<img className="button-icon" src={reactLogo} alt="" />
-								Learn more
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div id="social">
-					<svg className="icon" role="presentation" aria-hidden="true">
-						<use href="/icons.svg#social-icon"></use>
-					</svg>
-					<h2>Connect with us</h2>
-					<p>Join the Vite community</p>
-					<ul>
-						<li>
-							<a href="https://github.com/vitejs/vite" target="_blank">
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
-									<use href="/icons.svg#github-icon"></use>
-								</svg>
-								GitHub
-							</a>
-						</li>
-						<li>
-							<a href="https://chat.vite.dev/" target="_blank">
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
-									<use href="/icons.svg#discord-icon"></use>
-								</svg>
-								Discord
-							</a>
-						</li>
-						<li>
-							<a href="https://x.com/vite_js" target="_blank">
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
-									<use href="/icons.svg#x-icon"></use>
-								</svg>
-								X.com
-							</a>
-						</li>
-						<li>
-							<a href="https://bsky.app/profile/vite.dev" target="_blank">
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
-									<use href="/icons.svg#bluesky-icon"></use>
-								</svg>
-								Bluesky
-							</a>
-						</li>
-					</ul>
-				</div>
-			</section>
-
-			<div className="ticks"></div>
-			<section id="spacer"></section>
-		</>
-	)
+	return <div>
+		<h1>Something</h1>
+		<div className='flex justify-center'>
+			<div className='flex flex-col gap-4'>
+				{ data?.list?.map(({ key, label, value }) => {
+					const k = `ipt:${key}`
+					return <div className='flex flex-col p-4 rounded-[12px] border-black border-1 [&_input]:px-4 [&_input]:py-2'>
+						<label htmlFor={k}>{ label }</label>
+						<input id={k} type="text" value={value} onChange={e => dop.getByKey('list', key??'').update('list', { value: e.target.value })} />
+					</div>
+				}) ?? [] }
+			</div>
+		</div>
+	</div>
 }
 
-export default App
+export default function Wrapper(){
+	return <Provider>
+		<App />
+	</Provider>
+}
