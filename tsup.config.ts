377284af1e8bf89fbda308e654f669ee32869c43
Higor <hfashigor@hotmail.com>
2026-04-24 17:24:19 -0300

    Adjusts

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+// tsup.config.ts
+import { defineConfig } from 'tsup'
+
+export default defineConfig(
+    {
+        entry: {
+            provider: 'src/lib/complex_form/exports/provider.tsx',
+            hooks: 'src/lib/complex_form/exports/hooks.tsx',
+        },
+        format: ['esm', 'cjs'],
+        dts: true,
+        splitting: true,
+        treeshake: true,
+        clean: true,
+        external: ['react', 'react-dom'],
+        jsx: 'transform',
+        jsxImportSource: 'react',
+    }
+)
