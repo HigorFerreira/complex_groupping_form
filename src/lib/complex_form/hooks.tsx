cff8f84b3ca14f399e36b97a7aaa552540e0055b
Higor <hfashigor@gmail.com>
2026-05-28 14:56:10 -0300

    Clean refactoring

--- src/lib/complex_form/hooks.tsx
+++ src/lib/complex_form/hooks.tsx
@@ -1,4 +1,4 @@
-import { useContext as useReactContext,/* useEffect, useMemo, useRef, */type Context, useCallback, useMemo } from 'react'
+import { useContext as useReactContext,/* useEffect, useMemo, useRef, */type Context, useCallback, useMemo, useEffect } from 'react'
 import type { ContextType, BaseItem, RecordArrayGroups } from './types'
 // import { DataOperations } from './utils'
 import { v4 as uuidv4 } from 'uuid'
@@ -27,10 +27,22 @@ export function makeHooks<TGroups extends string, TItem extends Partial<BaseItem
     }
 
     function useData() {
-        const { counter, data } = useContext('useData', Context)
-        return useMemo(() => {
-            // O counter controla quando atualizar os dados no UI
-            return data?.current
+        const { counter, data: ref } = useContext('useData', Context)
+        const data = ref?.current
+        if(!data) throw new Error('No data')
+        
+        return useCallback((key?: string) => {
+            if(!key) return JSON.parse(JSON.stringify(data)) as RecordArrayGroups<TGroups, TItem>
+            for(const group_key in data) {
+                const find_index = data[group_key]?.findIndex(({ key: k }) => k === key)
+
+                if(find_index === undefined || find_index < 0) continue
+
+                return JSON.parse(JSON.stringify(
+                    data?.[group_key]?.[find_index]??{}
+                )) as TItem
+            }
+            return {} as TItem
         }, [ counter, data ])
     }
 
@@ -52,35 +64,37 @@ export function makeHooks<TGroups extends string, TItem extends Partial<BaseItem
 
     function useRemove(){
         const { setStructureCounter, setCounter, data: ref } = useContext('useRemove', Context)
-        function remove(key: string): boolean {
+        function remove(key: string) {
             const data = ref?.current
             if(!data) throw new Error('No data')
             for(const _group_key of Object.keys(data)){
                 const group_key = _group_key as TGroups
                 const remove_index = data[group_key]?.findIndex(({ key: k }) => k === key)
                 if(remove_index === undefined || remove_index < 0) continue
-                data[group_key]?.splice(remove_index, 1)
+                const [ deleted ] = data[group_key]?.splice(remove_index, 1) as Array<TItem>
                 setStructureCounter?.(prev => prev+1)
                 setCounter?.(prev => prev+1)
-                return true
+                return deleted
             }
-            return false
+            return {} as TItem
         }
         return remove
     }
 
     function useSetInitialData(data: RecordArrayGroups< TGroups, TItem >, maxRenders: number = 1) {
         const { setStructureCounter, setCounter, data: ref_object, initial } = useContext('useSetInitialData', Context)
-        if(initial?.current.renders){
-            initial.current.renders += 1
-        }
-        if(ref_object?.current) {
-            ref_object.current = data
-            if(initial?.current.renders??Number.MAX_SAFE_INTEGER >= maxRenders){
-                setStructureCounter?.(prev => prev+1)
-                setCounter?.(prev => prev+1)
+        
+        useEffect(() => {
+            initial?.current && (initial.current.renders += 1)
+            if(ref_object?.current !== undefined) {
+                ref_object.current = data
+                if((initial?.current.renders ?? Number.MAX_SAFE_INTEGER) >= maxRenders){
+                    setStructureCounter?.(prev => prev+1)
+                    setCounter?.(prev => prev+1)
+                }
             }
-        }
+        }, [ initial, ref_object ])
+
         const reset = useCallback(() => {
             if(initial?.current && initial.current.renders >= maxRenders){
                 initial.current.renders = 0
